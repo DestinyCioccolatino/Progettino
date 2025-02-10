ccdf42ebbb95626710cbbd442fa8b912ab78cfcc
DestinyCioccolatino <odia.destiny.2004@gmail.com>
2025-02-10 11:53:55 +0100

    saluto dal ramo cioccolatino

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! seconda versione");
+        System.out.println("Hello World! seconda versione ramo destinocioccolatino");
+
     }
 }
